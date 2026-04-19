da2c415db55f854fd7371f04bbd381189e3c99d2
matteo <mattegiorgi@gmail.com>
2026-04-19 16:36:25 +0200

    improved code formatting

--- test_binaryheap.c
+++ test_binaryheap.c
@@ -36,22 +36,22 @@ static void test_empty_heap(void)
 static void test_push_pop_order(void)
 {
     struct binaryheap heap;
-    int values[] = {7, 3, 9, 1, 4, 8, 2};
-    int expected[] = {1, 2, 3, 4, 7, 8, 9};
+    int values[] = { 7, 3, 9, 1, 4, 8, 2 };
+    int expected[] = { 1, 2, 3, 4, 7, 8, 9 };
     size_t i;
 
-    binaryheap_init (&heap, int_cmp);
+    binaryheap_init(&heap, int_cmp);
 
-    for (i = 0; i < sizeof (values) / sizeof (values[0]); i++)
+    for (i = 0; i < sizeof(values) / sizeof(values[0]); i++)
         assert(binaryheap_push(&heap, &values[i]) == 0);
 
     assert(binaryheap_size(&heap) == sizeof(values) / sizeof(values[0]));
-    assert(*(int *)binaryheap_peek(&heap) == 1);
+    assert(*(int *) binaryheap_peek(&heap) == 1);
 
     for (i = 0; i < sizeof(expected) / sizeof(expected[0]); i++)
-        assert(*(int *)binaryheap_pop(&heap) == expected[i]);
+        assert(*(int *) binaryheap_pop(&heap) == expected[i]);
 
-    assert(binaryheap_empty (&heap));
+    assert(binaryheap_empty(&heap));
 
     binaryheap_destroy(&heap);
 }
@@ -60,8 +60,8 @@ static void test_push_pop_order(void)
 static void test_duplicates(void)
 {
     struct binaryheap heap;
-    int values[] = {5, 1, 5, 1, 3};
-    int expected[] = {1, 1, 3, 5, 5};
+    int values[] = { 5, 1, 5, 1, 3 };
+    int expected[] = { 1, 1, 3, 5, 5 };
     size_t i;
 
     binaryheap_init(&heap, int_cmp);
@@ -69,7 +69,7 @@ static void test_duplicates(void)
     for (i = 0; i < sizeof(values) / sizeof(values[0]); i++)
         assert(binaryheap_push(&heap, &values[i]) == 0);
     for (i = 0; i < sizeof(expected) / sizeof(expected[0]); i++)
-        assert(*(int *)binaryheap_pop(&heap) == expected[i]);
+        assert(*(int *) binaryheap_pop(&heap) == expected[i]);
 
     binaryheap_destroy(&heap);
 }
@@ -78,9 +78,9 @@ static void test_duplicates(void)
 static void test_build(void)
 {
     struct binaryheap heap;
-    int values[] = {10, 6, 12, 2, 4};
-    void *items[sizeof (values) / sizeof (values[0])];
-    int expected[] = {2, 4, 6, 10, 12};
+    int values[] = { 10, 6, 12, 2, 4 };
+    void *items[sizeof(values) / sizeof(values[0])];
+    int expected[] = { 2, 4, 6, 10, 12 };
     size_t i;
 
     for (i = 0; i < sizeof(values) / sizeof(values[0]); i++)
@@ -89,10 +89,10 @@ static void test_build(void)
     binaryheap_build(&heap, items, sizeof(items) / sizeof(items[0]), int_cmp);
 
     assert(binaryheap_size(&heap) == sizeof(items) / sizeof(items[0]));
-    assert(*(int *)binaryheap_peek(&heap) == 2);
+    assert(*(int *) binaryheap_peek(&heap) == 2);
 
     for (i = 0; i < sizeof(expected) / sizeof(expected[0]); i++)
-        assert(*(int *)binaryheap_pop(&heap) == expected[i]);
+        assert(*(int *) binaryheap_pop(&heap) == expected[i]);
 }
 
 
